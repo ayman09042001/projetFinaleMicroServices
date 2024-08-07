fa8524ffad66321dffdef4995ce251ce63478c3f
ayman09042001 <AymanEddahbi@teccart.online>
2024-08-07 00:27:38 -0400

    chnager l'adresse host

--- backend/medecins_service.py
+++ backend/medecins_service.py
@@ -153,4 +153,4 @@ async def supprimer_horaire(id: int, horaire_id: int):
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host='127.0.0.1', port=8803)
+    uvicorn.run(app, host='0.0.0.0', port=8803)
